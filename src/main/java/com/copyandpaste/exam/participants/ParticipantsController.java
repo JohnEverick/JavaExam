258645b182c104da781df9a435a6cca7d7322343
John Everick Catayas <johheverick@gmail.com>
2020-04-27 22:41:54 +0800

    removed unused imports

--- src/main/java/com/copyandpaste/exam/participants/ParticipantsController.java
+++ src/main/java/com/copyandpaste/exam/participants/ParticipantsController.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
